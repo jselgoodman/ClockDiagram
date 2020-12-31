539f5d11a70100f2db2bde36d19a1119d0322d30
jselgoodman <65514923+jselgoodman@users.noreply.github.com>
2020-12-31 15:15:15 -0500

    Add files via upload

---
+++ ClockDiagram.java
@@ -0,0 +1,10 @@
+/**
+ * Purpose: Flip notes on an axis according to clock diagram
+ * @author: Jalen Goodman
+ * @version: 12/31/20
+ */
+public class ClockDiagram {
+    public static void main(String args[]){
+        System.out.println("Hello World");
+    }//end of main
+}
